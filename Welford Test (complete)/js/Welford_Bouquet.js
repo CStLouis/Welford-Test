c2132065c00be0f733096a08fee20a61b13e2dde
CStLouis <cstlouis1@sheffield.ac.uk>
2019-06-30 18:46:13 +0100

    Add files via upload
    Welford Test folder

---
+++ Welford Test (complete)/js/Welford_Bouquet.js
@@ -0,0 +1,18 @@
+// When user clicks the button, show dropdown menu
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// When user clicks out of content, close drop down menu
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
